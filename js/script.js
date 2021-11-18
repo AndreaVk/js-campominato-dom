f4aa6da8f5780f0b4ad87c90476c1f31e4c611a1
AndreaVk <vico.ac1996@gmail.com>
2021-11-18 23:54:07 +0100

    gioco bozza

---
+++ js/script.js
@@ -0,0 +1,80 @@
+document.getElementById('play').addEventListener('click', function(){
+    play();
+});
+
+function play(){
+    document.querySelector('.container-square').innerHTML='';
+    const levelSelected = parseInt(document.getElementById('level').value);
+    console.log(levelSelected);
+
+    const bombsNumber= 16; 
+    let cellsNumber, cellsForSide;
+
+    switch(levelSelected){
+        case 1:
+            cellsNumber = 100;
+            break;
+            case 2: 
+                cellsNumber = 81;
+                break;
+            case 3: 
+                cellsNumber = 49;
+        }
+
+        const bombs = generateBoms();
+    console.log(bombs);
+
+    function generateBoms() {
+        const arrayBombs = [];
+
+        while(arrayBombs.length < bombsNumber){
+            const numeroRandom = getRndInteger(1,cellsNumber);
+            if(!arrayBombs.includes(numeroRandom)) {
+                arrayBombs.push(numeroRandom);
+            }
+        }
+
+        return arrayBombs;
+
+    }
+
+    function getRndInteger(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
+    }
+
+    cellForSide = Math.sqrt(cellsNumber);
+    
+
+    generatePlayground();
+
+    function generatePlayground() {
+
+
+        const box = document.querySelector('.container-square');
+
+        for(let i = 1; i<= cellsNumber; i++){
+            const grid = createItem(i);
+
+            grid.addEventListener('click', function() {
+                this.classList.add('selected');
+            });
+
+            box.appendChild(grid);
+        }
+                
+    }
+
+    function createItem(num) {
+        const cell = document.createElement('div');
+        cell.classList.add('square');
+        const size = `calc(100% / ${cellForSide})`;
+        cell.style.width = size;
+        cell.style.height = size;
+
+        cell.innerHTML = num;
+
+        return cell;
+
+    }
+
+}
